2663e50430c656432bef8f7f86abf438f13fa4f9
Shin Matsuda <syncbunny@gmail.com>
2021-10-25 00:31:16 +0900

    (WIP) renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -17,6 +17,7 @@ use sdl2::event::Event;
 use sdl2::video::GLProfile;
 use sdl2::keyboard::Keycode;
 use sdl2::video::Window;
+use sdl2::video::GLContext;
 use gl::types::{GLuint};
 use crate::io::*;
 
@@ -24,10 +25,12 @@ pub struct Renderer {
 	io: Arc<Mutex<IO>>,
 	tex_id: u32,
 	vao: u32,
+	vbo: u32,
 	tex_data: Vec<u8>,
 	sdl_context: Sdl,
 	shader_program: u32,
 	window: Window,
+	gl_context: GLContext,
 }
 
 impl Renderer {
@@ -50,19 +53,20 @@ impl Renderer {
 		debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
 		debug_assert_eq!(gl_attr.context_version(), (3, 3));
 
-
 		let mut ret = Renderer {
 			io: io,
 			tex_id: 0,
 			vao: 0,
+			vbo: 0,
 			tex_data: vec![0; 256*240*3],
 			shader_program: 0,
 			sdl_context: sdl_context,
+			gl_context: ctx,
 			window: window
 		};
 
-		ret.check_gl_error();
-		//ret.init_gl();
+		ret.init_gl();
+		ret.check_gl_error(line!());
 		return ret;
 	}
 
@@ -70,16 +74,16 @@ impl Renderer {
 		let mut event_pump = self.sdl_context.event_pump().unwrap();
 
 		'running: loop {
-			self.check_gl_error();
+			self.check_gl_error(line!());
 			unsafe {
 				gl::ClearColor(0.6, 0.0, 0.8, 1.0);
-				self.check_gl_error();
+				self.check_gl_error(line!());
 				gl::Clear(gl::COLOR_BUFFER_BIT);
-				self.check_gl_error();
+				self.check_gl_error(line!());
 			}
 
         		self.window.gl_swap_window();
-			self.check_gl_error();
+			self.check_gl_error(line!());
 			{
 				//let mut io = self.io.lock().unwrap();
 		//		self.tex_data[0..].copy_from_slice(&io.vram[0..]);		
@@ -113,31 +117,48 @@ impl Renderer {
 		unsafe {
 			// create texture
 			gl::GenTextures(1, &mut self.tex_id);
+			self.check_gl_error(line!());
 			gl::BindTexture(gl::TEXTURE_2D, self.tex_id);
+			self.check_gl_error(line!());
 
 			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+			self.check_gl_error(line!());
 			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+			self.check_gl_error(line!());
 			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+			self.check_gl_error(line!());
 
 			gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, 256,  240, 0, gl::RGB, gl::UNSIGNED_BYTE, self.tex_data.as_ptr() as *const u8 as *const c_void);
+			self.check_gl_error(line!());
 
 			// create object
-			let position_data:[f32;12] = [
+			let position_data:Vec<f32> = vec![
 				-1.0,  1.0, 0.0,
 				-1.0, -1.0, 0.0,
 				 1.0,  1.0, 0.0,
 				 1.0, -1.0, 0.0,
 			];
 			gl::GenVertexArrays(1, &mut self.vao);
+			self.check_gl_error(line!());
 			gl::BindVertexArray(self.vao);
-			gl::BindBuffer(gl::ARRAY_BUFFER, self.vao);
-			gl::BufferData(gl::ARRAY_BUFFER, ((mem::size_of::<f32> as u32)*12) as isize, &position_data[0] as *const f32 as *const c_void, gl::STATIC_DRAW);
+			self.check_gl_error(line!());
+
+			gl::GenBuffers(1, &mut self.vbo);
+			self.check_gl_error(line!());
+			gl::BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+			self.check_gl_error(line!());
+			gl::BufferData(gl::ARRAY_BUFFER, (mem::size_of::<f32>()*12) as isize, position_data.as_ptr() as *const c_void, gl::STATIC_DRAW);
+			self.check_gl_error(line!());
 
 			gl::EnableVertexAttribArray(0);
+			self.check_gl_error(line!());
 			gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 0, 0 as *const c_void);
+			self.check_gl_error(line!());
 
 			gl::BindVertexArray(0);
+			self.check_gl_error(line!());
 			gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+			self.check_gl_error(line!());
 		}
 		self.create_program();
 	}
@@ -173,22 +194,30 @@ impl Renderer {
 			println!("compile vertex shader");
 			let vobj = gl::CreateShader(gl::VERTEX_SHADER);
 			gl::ShaderSource(vobj, 1, &mut vs_src.as_ptr(), ptr::null());
+			self.check_gl_error(line!());
 			gl::CompileShader(vobj);
+			self.check_gl_error(line!());
 			self.print_shader_log(vobj, "VertexShader");
 			println!("done.");
 			gl::AttachShader(self.shader_program, vobj);
+			self.check_gl_error(line!());
 			gl::DeleteShader(vobj);
+			self.check_gl_error(line!());
 		}
 
 		unsafe {
 			println!("compile fragment shader");
 			let vobj = gl::CreateShader(gl::FRAGMENT_SHADER);
 			gl::ShaderSource(vobj, 1, &mut fs_src.as_ptr(), ptr::null());
+			self.check_gl_error(line!());
 			gl::CompileShader(vobj);
+			self.check_gl_error(line!());
 			self.print_shader_log(vobj, "FragmentShader");
 			println!("done.");
 			gl::AttachShader(self.shader_program, vobj);
+			self.check_gl_error(line!());
 			gl::DeleteShader(vobj);
+			self.check_gl_error(line!());
 		}
 		
 	}
@@ -212,7 +241,7 @@ impl Renderer {
 		return buf_size;
 	}
 
-	fn check_gl_error(&self) {
+	fn check_gl_error(&self, line: u32) {
 		let err: u32;
 		unsafe {
 			err = gl::GetError();
@@ -220,22 +249,22 @@ impl Renderer {
 		match err {
 			gl::NO_ERROR => {}
 			gl::INVALID_ENUM => {
-				println!("GL_INVALID_ENUM");
+				println!("GL_INVALID_ENUM:{}", line);
 			}
 			gl::INVALID_VALUE => {
-				println!("GL_INVALID_VALUE");
+				println!("GL_INVALID_VALUE:{}", line);
 			}
-			gl::INVALID_VALUE => {
-				println!("GL_INVALID_OPERATION");
+			gl::INVALID_OPERATION => {
+				println!("GL_INVALID_OPERATION:{}", line);
 			}
 			gl::INVALID_FRAMEBUFFER_OPERATION => {
-				println!("GL_INVALID_FRAMEBUFFER_OPERATION");
+				println!("GL_INVALID_FRAMEBUFFER_OPERATION:{}", line);
 			}
 			gl::OUT_OF_MEMORY => {
-				println!("GL_OUT_OF_MEMORY");
+				println!("GL_OUT_OF_MEMORY:{}", line);
 			}
 			_ => {
-				println!("GL: unknown error: {}", err);
+				println!("GL: unknown error:{}:{}", line, err);
 			}
 		}
 	}
